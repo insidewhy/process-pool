d9cde6b267ad548edc6db10679bf9e1767f2ccf2
James Pike <jpike@chilon.net>
2015-03-24 00:04:11 +0000

    subprocess functions can delay exit with a promise.

--- src/test/ProcessPool.spec.js
+++ src/test/ProcessPool.spec.js
@@ -1,11 +1,23 @@
-var ProcessPool = require ('../ProcessPool')
+import Promise from 'bluebird'
+import ProcessPool from '../ProcessPool'
 
 describe('process pool', () => {
-  it('runs a single subprocess and returns the return value', () => {
+  it('should create a sub-process that can accept arguments and return a value', () => {
     var pool = new ProcessPool
     var func = pool.prepare(() => (arg1, arg2) => arg1 * arg2 * 10)
     return func(2, 3).then(v => {
       v.should.equal(60)
     })
   })
+
+  it(
+    'should create a sub-process that can accept arguments and return a value from a Promise',
+    () => {
+      var pool = new ProcessPool
+      var func = pool.prepare(() => (arg1, arg2) => Promise.resolve(arg1 * arg2 * 10))
+      return func(2, 3).then(v => {
+        v.should.equal(60)
+      })
+    }
+  )
 })
